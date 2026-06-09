f91358d9f04f9893ae97f250deb02def0f8a615a
Vincent <vincodev@dev-cube.net>
2026-06-09 12:57:09 +0200

    Finished 4.1.1 (style, client/server/models scripts, merged index.js, package.json and index.ejs)

---
+++ Aufgabe4/gta_v4/models/geotag-store.js
@@ -0,0 +1,87 @@
+// File origin: VS1LAB A3
+const GeoTag = require("./geotag.js");
+const GeoTagExamples = require("./geotag-examples.js");
+/**
+ * This script is a template for exercise VS1lab/Aufgabe3
+ * Complete all TODOs in the code documentation.
+ */
+
+/**
+ * A class for in-memory-storage of geotags
+ *
+ * Use an array to store a multiset of geotags.
+ * - The array must not be accessible from outside the store.
+ *
+ * Provide a method 'addGeoTag' to add a geotag to the store.
+ *
+ * Provide a method 'removeGeoTag' to delete geo-tags from the store by name.
+ *
+ * Provide a method 'getNearbyGeoTags' that returns all geotags in the proximity of a location.
+ * - The location is given as a parameter.
+ * - The proximity is computed by means of a radius around the location.
+ *
+ * Provide a method 'searchNearbyGeoTags' that returns all geotags in the proximity of a location that match a keyword.
+ * - The proximity constrained is the same as for 'getNearbyGeoTags'.
+ * - Keyword matching should include partial matches from name or hashtag fields.
+ */
+class InMemoryGeoTagStore {
+
+    /** @type {GeoTag[]} */
+    #geoTagStore = []; // nur in Store
+
+    /**
+     * Add a geotag to the store
+     * @param {GeoTag} geoTag The tag to add
+     */
+
+    addGeoTag(geotag) {
+        this.#geoTagStore.push(geotag);
+    }
+
+    /**
+     * Remove a geo-tag from the store by name
+     * @param {string} geoTagName The name of the geo-tag to remove
+     */
+
+    removeGeoTag(name) {
+        this.#geoTagStore = this.#geoTagStore.filter(geotag => geotag.name !== name);
+    }
+    /**
+     * Get all geotags in the proximity of a location.
+     * @param {int} latitude Latitude of the location
+     * @param {int} longitude Longitude of the location
+     * @param {int} radius Proximity around the location.
+     * @return {GeoTag[]} Array of GeoTags found.
+     */
+
+    getNearbyGeoTags(latitude, longitude, radius) {
+        return this.#geoTagStore.filter(geotag => {
+            const distance = this.calculateDistance(latitude, longitude, geotag.latitude, geotag.longitude);
+            return distance <= radius;
+        });
+    }
+
+    calculateDistance(lat1, lon1, lat2, lon2) {
+        const dLat = lat1 - lat2;
+        const dLon = lon1 - lon2;
+        return Math.sqrt(dLat * dLat + dLon * dLon);
+    }
+
+    /**
+     * Search for geotags in the proximity of a location that match a keyword.
+     * @param {string} keyword Keyword to search for in name and hashtag fields of geotags
+     * @param {int} distance Proximity around the location.
+     * @return {GeoTag[]} Array of GeoTags found.
+     */
+
+
+    searchNearbyGeoTags(latitude, longitude, radius, keyword) {
+        const term = keyword.toLowerCase();
+        return this.getNearbyGeoTags(latitude, longitude, radius).filter(geotag => {
+            return geotag.name.toLowerCase().includes(term)
+                || geotag.hashtag.toLowerCase().includes(term);
+        });
+    }
+}
+
+module.exports = InMemoryGeoTagStore
--- Aufgabe4/gta_v4/routes/index.js
+++ Aufgabe4/gta_v4/routes/index.js
@@ -26,6 +26,16 @@ const GeoTag = require('../models/geotag');
  */
 // eslint-disable-next-line no-unused-vars
 const GeoTagStore = require('../models/geotag-store');
+const GeoTagExamples = require('../models/geotag-examples');
+
+const SEARCH_RADIUS = 0.1;
+
+const store = new GeoTagStore();
+
+// Beispieldaten in Store laden
+GeoTagExamples.tagList.forEach(([name, latitude, longitude, hashtag]) => {
+  store.addGeoTag(new GeoTag(name, latitude, longitude, hashtag));
+});
 
 // App routes (A3)
 
@@ -39,7 +49,74 @@ const GeoTagStore = require('../models/geotag-store');
  */
 
 router.get('/', (req, res) => {
-  res.render('index', { taglist: [] })
+  res.render('index', {
+    taglist: [],
+    latitude: '',
+    longitude: ''
+  });
+});
+
+/**
+ * Route '/tagging' for HTTP 'POST' requests.
+ * (http://expressjs.com/de/4x/api.html#app.post.method)
+ *
+ * Requests cary the fields of the tagging form in the body.
+ * (http://expressjs.com/de/4x/api.html#req.body)
+ *
+ * Based on the form data, a new geotag is created and stored.
+ *
+ * As response, the ejs-template is rendered with geotag objects.
+ * All result objects are located in the proximity of the new geotag.
+ * To this end, "GeoTagStore" provides a method to search geotags
+ * by radius around a given location.
+ */
+
+
+router.post('/tagging', (req, res) => {
+  var name = req.body.name ? req.body.name : "";
+  var latitude = req.body.latitude ?
+      (parseFloat(req.body.latitude)).toFixed(5) : 49.01158;
+  var longitude = req.body.longitude ?
+      (parseFloat(req.body.longitude)).toFixed(5) : 8.39343;
+  var hashTag = req.body.hashtag ? req.body.hashtag : "";
+
+  var geoTag = new GeoTag(name, latitude, longitude, hashTag);
+  store.addGeoTag(geoTag);
+  var tags = store.getNearbyGeoTags(latitude, longitude, SEARCH_RADIUS);
+
+  res.render('index', {
+    taglist: tags, latitude: latitude, longitude: longitude
+  });
+});
+
+/**
+ * Route '/discovery' for HTTP 'POST' requests.
+ * (http://expressjs.com/de/4x/api.html#app.post.method)
+ *
+ * Requests cary the fields of the discovery form in the body.
+ * This includes coordinates and an optional search term.
+ * (http://expressjs.com/de/4x/api.html#req.body)
+ *
+ * As response, the ejs-template is rendered with geotag objects.
+ * All result objects are located in the proximity of the given coordinates.
+ * If a search term is given, the results are further filtered to contain
+ * the term as a part of their names or hashtags.
+ * To this end, "GeoTagStore" provides methods to search geotags
+ * by radius and keyword.
+ */
+
+router.post('/discovery', (req, res) => {
+  var searchterm = req.body.searchterm ? req.body.searchterm : "";
+  var latitude = req.body.latitude ?
+      (parseFloat(req.body.latitude)).toFixed(5) : 49.01158;
+  var longitude = req.body.longitude ?
+      (parseFloat(req.body.longitude)).toFixed(5) : 8.39343;
+
+  var tags = store.searchNearbyGeoTags(latitude, longitude, SEARCH_RADIUS, searchterm);
+
+  res.render('index', {
+    taglist: tags, latitude: latitude, longitude: longitude
+  });
 });
 
 // API routes (A4)
